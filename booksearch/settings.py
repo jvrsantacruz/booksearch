40691f0e843383f5014c66ac1022d19ee72f69a6
Javier Santacruz <javier.santacruz.lc@gmail.com>
2012-07-15 22:18:05 +0200

    Adds settings.py file
    This file contains the main information to connect to the API's
    my own keys has been deleted from them, they should be replaced by other
    ones by going to the pages and registering yourself. It's easy.

---
+++ booksearch/settings.py
@@ -0,0 +1,15 @@
+# max number of items in cache
+CACHE_THRESHOLD = 1000
+
+# cache invalidation time (1h)
+CACHE_TIME = 3600
+
+# Google Books API Key
+GOOGLE_BOOKS_ACCESS_KEY = ''
+
+# Amazon Services Public and Private Key
+AMAZON_ACCESS_KEY = ''
+AMAZON_SECRET_ACCESS_KEY = ''
+
+# ISBNdb access token
+ISBNdb_ACCESS_KEY = '';
